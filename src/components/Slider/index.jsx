10ccf371859527333e47c3676011d8f094a00020
Victor Mal <VictorianusVelikyu@gmail.com>
2021-10-19 17:04:24 +0300

    Add control buttons

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Button from "../Button/index";
 
 const arrayImages = [
   "https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-cosmic-reef-2400x1200.jpg?t=tn992",
@@ -10,19 +11,41 @@ const arrayImages = [
 
 export default function Slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + arrayImages.length) % arrayImages.length
+    );
+  };
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % arrayImages.length);
+    console.log(currentSlide);
+  };
+  const autoSlide = () => {
+    setRunning((running) => !running);
+  };
 
   useEffect(() => {
+    if (!running) {
+      console.log(running);
+      return;
+    }
     const intervalID = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide + 1) % arrayImages.length);
+      console.log(currentSlide);
     }, 2000);
 
     return () => {
       clearInterval(intervalID);
     };
-  }, [currentSlide]);
+  }, [currentSlide, running]);
   return (
     <div>
-      <img src={arrayImages[currentSlide]} alt="" />
+      <Button title={"Prev"} action={prevSlide} />
+      <img src={arrayImages[currentSlide]} alt="slide" />
+      <Button title={"Next"} action={nextSlide} />
+      <Button title={"Slide Show"} action={autoSlide} />
     </div>
   );
 }
