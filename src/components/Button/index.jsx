6e11ec937c623ad5f1d74d54c154779a2a09733d
Victor Mal <VictorianusVelikyu@gmail.com>
2021-10-19 17:02:35 +0300

    Add Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Button(props) {
+  return (
+    <button style={props.style} onClick={props.action}>
+      {props.title}
+    </button>
+  );
+}
