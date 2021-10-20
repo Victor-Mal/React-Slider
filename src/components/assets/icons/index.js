011cfc82c54994b9a69bbbc7fc910ac8f8500262
Victor Mal <VictorianusVelikyu@gmail.com>
2021-10-20 21:49:42 +0300

    Add svg and svg js export

---
+++ src/components/assets/icons/index.js
@@ -0,0 +1,4 @@
+export {default as fullscreen} from './fullscreen.svg';
+export {default as left_arrow} from './left_arrow.svg';
+export {default as right_arrow} from './right_arrow.svg';
+export {default as run} from './run.svg' 
