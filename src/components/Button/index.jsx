35b5afc77f286fc72114acb0f193b2e1b27ddc58
Victor Mal <VictorianusVelikyu@gmail.com>
2021-10-21 01:02:15 +0300

    Add className to Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button(props) {
   return (
-    <button style={props.style} onClick={props.action}>
+    <button className={props.className} style={props.style} onClick={props.action}>
       {props.title}
     </button>
   );
